ecee77cbc96457095db2bab69d09fc0ad077aff4
CarolinaMons <carolinamosalvev@hotmail.com>
2020-05-07 10:10:14 -0500

    Aplicación EUEstudiante

--- EUEstudiante/app/src/main/java/com/example/euestudiante/ActividadActivity.java
+++ EUEstudiante/app/src/main/java/com/example/euestudiante/ActividadActivity.java
@@ -1,43 +1,135 @@
 package com.example.euestudiante;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
+import java.util.ArrayList;
 
 public class ActividadActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
-    String[] myDataset = {"Actividad 1", "Actividad 2", "Actividad 3", "Actividad 4", "Actividad 5"};
+    private RecyclerView recyclerActividad;
+    private ArrayList<Actividad> actividads;
+    private String idSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad);
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_actividad);
 
-        recyclerView.setHasFixedSize(true);
+        permisos();
+        idSesion = MainActivity.ID;
+        recyclerActividad = findViewById(R.id.recycler_actividad);
+        recyclerActividad.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
 
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        //mAdapter = new MyAdapter(myDataset);
-        recyclerView.setAdapter(mAdapter);
-    }
-
-    public void BtnArchivo(View v){
-        /*Al dar click en un archivo abrir la actividad de archivo*/
-        Intent i = new Intent(this, ArchivoActivity.class);
-        startActivity(i);
+        actividads = new ArrayList<>();
+        consultar();
+        AdapterActividad mAdapter = new AdapterActividad(actividads);
+        recyclerActividad.setAdapter(mAdapter);
 
     }
 
-    public void BtnDescargar(View v){
-        /*Descargar el contenido de todas las actividades*/
+    /**
+     * Se asigna el menu de sincronizar al activity
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_sync, menu);
+        return true;
     }
+
+    /**
+     * Metodo cuando se unde en el menú
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                this.finish();
+                return true;
+            case R.id.action_sync:
+                Toast.makeText(getApplicationContext(),"Estados sincronizados",Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    /**
+     * Consultar las actividades de la sesion para ponerlas en el RecyclerView
+     */
+    private void consultar(){
+        String[][] data,files;
+        String fileNames;
+        int nImg, nDoc, nApk, nVideo, nAudio, nOther;
+        try {
+            //Consultar todas las actividades
+            data = MainActivity.apiRest.getData("Actividades");
+            for(int i=0; i< data.length; i++){
+                if(data[i][1].equals(idSesion)){// ID | Sesion_ID | Descripcion | Tiempo
+                    files = MainActivity.apiRest.getData("Recursos","Hipervinculo","Actividad_ID="+ data[i][0]);
+                    int k;
+                    for(k=0, nImg=0, nDoc=0, nApk=0, nVideo=0, nAudio=0, nOther=0; k< files.length; k++){// Hipervinculo
+                        fileNames = files[k][0].split("/")[files[k][0].split("/").length-1]; //Obtener solo el nombre
+
+                        if(fileNames.endsWith(".png")||fileNames.endsWith(".PNG")||fileNames.endsWith(".jpg")||fileNames.endsWith(".JPG")||
+                                fileNames.endsWith(".jpeg") ||fileNames.endsWith(".JPEG"))
+                            nImg++;
+                        else if(fileNames.endsWith(".docx")||fileNames.endsWith(".DOCX")||fileNames.endsWith(".pdf")||fileNames.endsWith(".PDF")||
+                                fileNames.endsWith(".txt")||fileNames.endsWith(".TXT"))
+                            nDoc++;
+                        else if(fileNames.endsWith(".apk") || fileNames.endsWith(".APK"))
+                            nApk++;
+                        else if(fileNames.endsWith(".mp4")||fileNames.endsWith(".MP4")||fileNames.endsWith(".mov")||fileNames.endsWith(".MOV"))
+                            nVideo++;
+                        else if(fileNames.endsWith(".mp3")||fileNames.endsWith(".MP3")||fileNames.endsWith(".ogg")||fileNames.endsWith(".OGG")||
+                                fileNames.endsWith(".wav")||fileNames.endsWith(".WAV"))
+                            nAudio++;
+                        else
+                            nOther++;
+                    }
+                    fileNames = nImg==0 ? "" : nImg==1 ? nImg+" imagen ":nImg+" imagenes ";
+                    fileNames += nDoc==0 ? "" : nDoc==1 ? nDoc+" documento " : nDoc+" documentos ";
+                    fileNames += nApk==0 ? "" : nApk==1 ? nApk+" aplicación " : nApk+" aplicaciones ";
+                    fileNames += nVideo==0 ? "" : nVideo==1 ? nVideo+" video " : nVideo+" videos ";
+                    fileNames += nAudio==0 ? "" : nAudio==1 ? nAudio+" audio " : nAudio+" audios ";
+                    fileNames += nOther==0 ? "" : nOther==1 ? nOther+" otro archivo " : nOther+" otros archivos ";
+
+                    actividads.add(new Actividad("Actividad "+(i+1), data[i][2], data[i][3]+" minutos", fileNames,
+                            (ImageButton)findViewById(R.id.btnDescargar),(ImageButton)findViewById(R.id.btnExpand), (ImageButton)findViewById(R.id.btnEstado)));
+                }
+            }
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error ApiRest: "+e.getMessage(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Metodo para para pedir los permisos de almacenamiento
+     */
+    private void permisos(){
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M){
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ) {
+                String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE};
+                requestPermissions(permissions,1);
+            }
+        }
+    }
+
 }
--- EUEstudiante/app/src/main/java/com/example/euestudiante/MainActivity.java
+++ EUEstudiante/app/src/main/java/com/example/euestudiante/MainActivity.java
@@ -2,22 +2,97 @@ package com.example.euestudiante;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static ConexionFTPS client;
+    public static ConexionApiRest apiRest;
+    public static String ID="0";
+    public static Context context;
+    private EditText idSesion, numIdent;
+    private Spinner tipoIdent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
+
+        context = getApplicationContext();
+        idSesion = findViewById(R.id.edit_sesion);
+        tipoIdent = findViewById(R.id.spinner_tipoIdent);
+        numIdent = findViewById(R.id.edit_numIdent);
     }
 
-    public void BtnSesion(View v){
+    /**
+     * Validar los campos para poder inicial sesion
+     * @param v
+     */
+    public void btnSesion(View v){
+        //Utilizar API para conexion con base de datos
         Intent i = new Intent(this, ActividadActivity.class);
-        startActivity(i);
+        if(idSesion.getText().toString().isEmpty())
+            Toast.makeText(getApplicationContext(), "Debe escribir un ID de sesion", Toast.LENGTH_SHORT).show();
+        else if(tipoIdent.getSelectedItemPosition()==0)
+            Toast.makeText(getApplicationContext(), "Debe selecionar un tipo de identificación", Toast.LENGTH_SHORT).show();
+        else if(numIdent.getText().toString().isEmpty())
+            Toast.makeText(getApplicationContext(), "Debe escribir un numero de identificación", Toast.LENGTH_SHORT).show();
+        else{
+            ID = idSesion.getText().toString();
+            if(connectMariaDB())
+                startActivity(i);
+        }
     }
 
+    /**
+     * Metodo para establecer la conexion con el servidor MariaDB por medio de la clase de ConexionApiRest
+     */
+    private boolean connectMariaDB(){
+        //Me conecto con el servidor de base de datos
+        String ftps, user, pass;
+        try {
+            apiRest = new ConexionApiRest(getString(R.string.mariadb));
+            String[][] data = apiRest.getData( "FTPS");
+            ftps = data[0][1];
+            user = data[0][2];
+            pass = data[0][3];
+            if(connectFTPS(ftps,user,pass)){
+                //Validar que la sesion y el estudiante existe
+                if(apiRest.getData("Sesiones","ID","ID="+ID).length == 1 &&
+                        apiRest.getData("Estudiante","ID","Identificacion="+numIdent.getText().toString()).length == 1)
+                    return true;
+
+                Toast.makeText(getApplicationContext(), "No existe la sesion con ID = "+ID, Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error ApiRest: "+e.getMessage(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * Metodo para establecer la conexion con el servidor SFTP por medio de la clase de ConexionFTPS
+     */
+    private boolean connectFTPS(String ftps, String user, String pass){
+        try {
+            client = new ConexionFTPS(ftps,user,pass);
+            client.connect();
+            client.disconnect();
+            return true;
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error FTPS: "+e.getMessage(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+        return false;
+    }
 
 }
