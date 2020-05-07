f0ac6e9a94b1e5fbd8092ee90d6d0dec2ac4f731
J Camilo Rpo V <juan.restrepov@upb.edu.co>
2020-05-07 12:17:38 -0500

    Back-end actividades de una sesión
    Manejo logica para mostrar todas las actividades de una sesion

--- EUProfesor/app/src/main/java/com/example/euprofesor/ActividadActivity.java
+++ EUProfesor/app/src/main/java/com/example/euprofesor/ActividadActivity.java
@@ -12,10 +12,9 @@ import java.util.ArrayList;
 
 public class ActividadActivity extends AppCompatActivity {
 
-    private String idSesion;
     private RecyclerView recyclerActividad;
-    //private ArrayList<Actividad> actividads;
-
+    private ArrayList<Actividad> actividads;
+    private String idSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,24 +25,24 @@ public class ActividadActivity extends AppCompatActivity {
         recyclerActividad= findViewById(R.id.recycler_actividades);
         recyclerActividad.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
 
-       /* actividads = new ArrayList<>();
+        actividads = new ArrayList<>();
         consultar();
         AdapterActividad mAdapter = new AdapterActividad(actividads);
-        recyclerActividad.setAdapter(mAdapter);*/
+        recyclerActividad.setAdapter(mAdapter);
     }
 
-    /*private void consultar(){
+    private void consultar(){
         String[][] data,files;
         String fileNames;
         int nImg, nDoc, nApk, nVideo, nAudio, nOther;
         try {
             //Consultar todas las actividades
             data = MainActivity.apiRest.getData("Actividades");
-            for(int i=0; i< data.length; i++){
+            for(int i=0,j=1; i< data.length; i++){
                 if(data[i][1].equals(idSesion)){// ID | Sesion_ID | Descripcion | Tiempo
                     files = MainActivity.apiRest.getData("Recursos","Hipervinculo","Actividad_ID="+ data[i][0]);
                     int k;
-                    for(k=0, nImg=0, nDoc=0, nApk=0, nVideo=0, nAudio=0, nOther=0; k< files.length; k++){// Hipervinculo
+                    for(k=0, nImg=0, nDoc=0, nApk=0, nVideo=0, nAudio=0, nOther=0; k < files.length; k++){// Hipervinculo
                         fileNames = files[k][0].split("/")[files[k][0].split("/").length-1]; //Obtener solo el nombre
 
                         if(fileNames.endsWith(".png")||fileNames.endsWith(".PNG")||fileNames.endsWith(".jpg")||fileNames.endsWith(".JPG")||fileNames.endsWith(".jpeg") ||fileNames.endsWith(".JPEG"))
@@ -59,19 +58,24 @@ public class ActividadActivity extends AppCompatActivity {
                         else
                             nOther++;
                     }
-                    fileNames = nImg==0 ? "" : nImg==1 ? nImg+" imagen ":nImg+" imagenes ";
-                    fileNames += nDoc==0 ? "" : nDoc==1 ? nDoc+" documento " : nDoc+" documentos ";
-                    fileNames += nApk==0 ? "" : nApk==1 ? nApk+" aplicación " : nApk+" aplicaciones ";
-                    fileNames += nVideo==0 ? "" : nVideo==1 ? nVideo+" video " : nVideo+" videos ";
-                    fileNames += nAudio==0 ? "" : nAudio==1 ? nAudio+" audio " : nAudio+" audios ";
-                    fileNames += nOther==0 ? "" : nOther==1 ? nOther+" otro archivo " : nOther+" otros archivos ";
+                    if(files.length>0){
+                        fileNames = nImg==0 ? "" : nImg==1 ? nImg+" imagen ":nImg+" imagenes ";
+                        fileNames += nDoc==0 ? "" : nDoc==1 ? nDoc+" documento " : nDoc+" documentos ";
+                        fileNames += nApk==0 ? "" : nApk==1 ? nApk+" aplicación " : nApk+" aplicaciones ";
+                        fileNames += nVideo==0 ? "" : nVideo==1 ? nVideo+" video " : nVideo+" videos ";
+                        fileNames += nAudio==0 ? "" : nAudio==1 ? nAudio+" audio " : nAudio+" audios ";
+                        fileNames += nOther==0 ? "" : nOther==1 ? nOther+" otro archivo " : nOther+" otros archivos ";
+                    }
+                    else{
+                        fileNames = "No hay archivos.";
+                    }
 
-                    actividads.add(new Actividad("Actividad "+(i+1), data[i][2], data[i][3]+" minutos", fileNames, (ImageButton) findViewById(R.id.btnExpand)));
+                    actividads.add(new Actividad("Actividad "+(j++), data[i][2], data[i][3]+" minutos", fileNames, (ImageButton) findViewById(R.id.btnExpand)));
                 }
             }
         } catch (Exception e) {
             Toast.makeText(getApplicationContext(), "Error ApiRest: "+e.getMessage(), Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
-    }*/
+    }
 }
--- EUProfesor/app/src/main/java/com/example/euprofesor/NewSesionActivity.java
+++ EUProfesor/app/src/main/java/com/example/euprofesor/NewSesionActivity.java
@@ -8,7 +8,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+
 import org.json.JSONException;
+
 import java.io.IOException;
 import java.security.InvalidKeyException;
 
@@ -22,30 +24,13 @@ public class NewSesionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_sesion);
+
+        proposito = findViewById(R.id.edit_Proposito);
+        areas = findViewById(R.id.sp_areas);
+        fechaInicio = findViewById(R.id.edit_fechainicio);
+        fechaCierre = findViewById(R.id.edit_fechacierre);
+        btnAgregar = findViewById(R.id.btn_agregar);
     }
 
-
-    /*Codigo para cargar archivos
-    public void btnCargar(final View v){
-        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-        i.setType("* /*");
-        i.addCategory(Intent.CATEGORY_OPENABLE); //Abrir el Choose File del dispositivo movil
-        startActivityForResult(Intent.createChooser(i,"Choose File"),1);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_CANCELED)
-            Toast.makeText(getApplicationContext(), "No se eligió un archivo", Toast.LENGTH_SHORT).show();
-        if ((resultCode == RESULT_OK) && (requestCode == 1)) {
-            //Procesar el resultado
-            Uri uri = data.getData(); //obtener el uri content
-            String ftpPath = "/home/admin/wwww";
-            String localFile = GetPathUtil.getPath(getApplicationContext(), uri);
-            String name = new File(localFile).getName();
-            new SimpleTask(new View(getApplicationContext()), false).execute(ftpPath.split("@"),name.split("@"),localFile.split("@"));
-        }
-    }
-    /**/
+    
 }
